ad62d316104ecdd758a33425c23a5969680d91fc
Ricardo Tan <ricardo.tan.2000@gmail.com>
2023-11-09 11:56:22 +0700

    save to memory

--- src/Attendance/modules/index.js
+++ src/Attendance/modules/index.js
@@ -4,7 +4,7 @@ const CONTROLLER = require('./controller');
 const { authMiddleware } = require('../../Middleware/middlewareAuth');
 
 // const upload = multer({ dest: `${__dirname}/file/attendance/` });
-const upload = multer({ dest: `${__dirname}/tmp/`});
+const upload = multer();
 const validate = require('../../Middleware/middlewareValidation');
 const schema = require('./validation');
 
