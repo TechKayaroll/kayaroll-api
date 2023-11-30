0837d985b73eb4ee6491092a82e83ecfdcb0513b
Ricardo Tan <ricardo.tan.2000@gmail.com>
2023-11-30 17:40:37 +0700

    remove unused middleware

--- src/Middleware/middlewareAuth.js
+++ src/Middleware/middlewareAuth.js
@@ -1,35 +1,8 @@
-const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const model = require('../Users/modules/model');
-const cipher = require('../../Helpers/encrypt');
 const { decodeToken } = require('../../Helpers/jwt');
 const struct = require('../Users/modules/struct');
 
-module.exports.authMiddleware = async (req, res, next) => {
-  const token = req.header('Authorization');
-  if (token === undefined || !token.startsWith('Bearer ')) {
-    res.status(StatusCodes.UNAUTHORIZED).json({
-      message: ReasonPhrases.UNAUTHORIZED,
-      data: {},
-      code: StatusCodes.UNAUTHORIZED,
-    });
-  } else {
-    const originToken = token.substring(7, token.length);
-    const dataCipher = await cipher.DecryptToken(originToken);
-    const user = await model.getDataUserMiddleware(dataCipher);
-    if (user === undefined || user === null) {
-      res.status(StatusCodes.UNAUTHORIZED).json({
-        message: ReasonPhrases.UNAUTHORIZED,
-        data: {},
-        code: StatusCodes.UNAUTHORIZED,
-      });
-    } else {
-      req.user = struct.MiddlewareUserResponse(user.userId, user.organizationId);
-      next();
-    }
-  }
-};
-
-// JWT Authentication
 module.exports.authentication = async (req, res, next) => {
   const headerToken = req.header('Authorization');
   try {
